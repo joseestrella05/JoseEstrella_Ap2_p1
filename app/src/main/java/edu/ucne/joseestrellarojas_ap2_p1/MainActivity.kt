8a9fd4992b672b585d2a2f0473f014edbacb6240
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-10-01 18:53:47 -0400

    Creacion del proyecto

---
+++ app/src/main/java/edu/ucne/joseestrellarojas_ap2_p1/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.ucne.joseestrellarojas_ap2_p1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.joseestrellarojas_ap2_p1.ui.theme.JoseEstrellaRojas_AP2_P1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JoseEstrellaRojas_AP2_P1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JoseEstrellaRojas_AP2_P1Theme {
+        Greeting("Android")
+    }
+}
